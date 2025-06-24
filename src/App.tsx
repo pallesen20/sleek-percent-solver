52226e0c93aed4b3ad598084a77eea9fe5098ab3
pallesen20 <jesperpallesen20@gmail.com>
2025-06-24 17:46:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/word-counter" element={<WordCounterPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>

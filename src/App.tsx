cbe03ffd71232bce24437e0a23a7bd5a6cd6b05e
pallesen20 <jesperpallesen20@gmail.com>
2025-07-24 14:29:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/header";
 import Index from "./pages/Index";
 import WordCounterPage from "./pages/WordCounterPage";
 import LengthUnitsPage from "./pages/conversion/LengthUnitsPage";

2618e6a0eac11444c60f2c86b88b096eed679e38
pallesen20 <jesperpallesen20@gmail.com>
2025-09-07 12:03:16 +0200

    Update header.js

--- header.js
+++ header.js
@@ -5,7 +5,9 @@ function loadHeader() {
         <nav class="nav-menu">
           <a href="/" class="nav-link">Percentage Calculator</a>
           <div class="nav-dropdown">
-            <a href="#" class="nav-link dropdown-toggle">Conversion ▼</a>
+            <a href="#" class="nav-link dropdown-toggle">Conversion<svg class="dropdown-arrow" viewBox="0 0 10 6">
+            <path fill="currentColor" fill-rule="evenodd" d="M9.354.646a.5.5 0 0 0-.708 0L5 4.293 1.354.646a.5.5 0 0 0-.708.708l4 4a.5.5 0 0 0 .708 0l4-4a.5.5 0 0 0 0-.708" clip-rule="evenodd"></path>
+          </svg></a>
             <div class="dropdown-content">
               <a href="/conversion/length-converter" class="dropdown-link">Length Converter</a>
               <a href="/conversion/weight-converter" class="dropdown-link">Weight Converter</a>

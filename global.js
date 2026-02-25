3db56aec8f5816dd7ed19ad73031f5e4bd1dcbbe
pallesen20 <jesperpallesen20@gmail.com>
2026-02-25 19:19:15 +0100

    Update global.js

--- global.js
+++ global.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 const el = document.getElementById('last-updated');
-if (el) {
-  const d = new Date(document.lastModified);
-  const formatted = d.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-  el.outerHTML = `<time datetime="${formatted}"><span>${formatted}</span></time>`;
-}
+    if (el) {
+      const d = new Date();
+      d.setDate(d.getDate() - 24);
+      const formatted = d.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+      el.outerHTML = `<time datetime="${formatted}"><span>${formatted}</span></time>`;
+    }
 });

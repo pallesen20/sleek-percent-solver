e4085119177a02ed13f8eee474dd97a1e7c7f9be
pallesen20 <jesperpallesen20@gmail.com>
2026-02-25 18:56:09 +0100

    Create global.js

---
+++ global.js
@@ -0,0 +1,6 @@
+const el = document.getElementById('last-updated');
+if (el) {
+  const d = new Date(document.lastModified);
+  const formatted = d.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+  el.outerHTML = `<time datetime="${formatted}"><span>${formatted}</span></time>`;
+}

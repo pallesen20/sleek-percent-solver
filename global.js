365e5507e21385bb493c0cb7ca579d8eb9ee1ab6
pallesen20 <jesperpallesen20@gmail.com>
2026-02-25 19:35:28 +0100

    Delete global.js

--- global.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-const el = document.getElementById('last-updated');
-    if (el) {
-      const d = new Date();
-      d.setDate(d.getDate() - 24);
-      const formatted = d.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-      el.outerHTML = `<time datetime="${formatted}"><span>${formatted}</span></time>`;
-    }
-});
